de6174ed13e02ef6524171c545b48e548d02a134
Mauricio Madrid Prado <mauriciomadridprado@gmail.com>
2023-06-24 14:38:07 -0300

    add: Login page

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,9 @@
 //efeito de esconder formulário de cadastro
+
+$(document).ready(function () {
+  $("#botao-cadastrar").click(function () {
+    $("#section-cadastro").slideToggle("slow");
+    $("#section-login").slideToggle("slow");
+    $("#botao-cadastrar").hide();
+  });
+});
--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 //efeito do botão voltar ao Topo
-
+function topo(){
+    window.scrollTo(
+        {
+            to:0,
+            left:0,
+            behavior:"smooth"
+        }
+    )
+}
 //Validação de Login
+function login(){
+    let logado = 0;
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+    if(usuario == 'admin' && senha == '123456'){
+        window.location = "index.html";
+        logado = 1;
+    }
+    if(logado == 0){
+        alert('Acesso Negado! Dados incorretos.')
+    }
 
+}
 //Ativar alert no botão cadastrar
+
+
+function cadastro(){
+    alert("Cadastro com sucesso!");
+    window.location.href = "index.html"
+}
